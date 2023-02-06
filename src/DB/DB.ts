aaed27f872128f7cc01be439d021f3864f3b4a39
LeshaPI <pialeksej5@gmail.com>
2023-02-06 16:14:50 +0300

    feat: tracks route

--- src/DB/DB.ts
+++ src/DB/DB.ts
@@ -5,11 +5,14 @@ import { v4 as uuidv4 } from 'uuid';
 import { CreateUserDto } from '../users/types/user.dto';
 import { Artist } from '../artists/types/artist.shema';
 import { ArtistDto } from '../artists/types/artist.dto';
+import { Track } from '../tracks/types/track.shema';
+import { createTrackDto } from '../tracks/types/track.dto';
 
 @Injectable()
 export default class DB {
   users: User[] = [];
   artists: Artist[] = [];
+  tracks: Track[] = [];
 
   getOneUser(id) {
     for (let user of this.users) {
@@ -74,6 +77,13 @@ export default class DB {
   deleteArtist(id: string) {
     const index = this.artists.findIndex((dbArtist) => dbArtist.id === id);
     const artist = this.artists[index];
+
+    for(let track of this.tracks) {
+      if(id === track.artistId){
+        track.artistId = null;
+      }
+    }
+
     if (artist) {
       this.artists.splice(index, 1);
     }
@@ -81,4 +91,38 @@ export default class DB {
     return artist;
   }
 
+  getOneTrack(id) {
+    for (let track of this.tracks) {
+      if (track.id === id) {
+        return track;
+      }
+    }
+  }
+
+
+  setTrack(trackDto: createTrackDto) {
+    const updatedTrack: Track = {
+      id: uuidv4(),
+      ...trackDto
+    }
+
+    this.tracks.push(updatedTrack);
+    return updatedTrack;
+  }
+
+  udateTrack(track: Track) {
+    const index = this.tracks.findIndex((dbTrack) => dbTrack.id === track.id);
+    this.tracks.splice(index, 1, track);
+  }
+
+  deleteTrack(id: string) {
+    const index = this.tracks.findIndex((dbTrack) => dbTrack.id === id);
+    const track = this.tracks[index];
+
+    if (track) {
+      this.tracks.splice(index, 1);
+    }
+
+    return track;
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Database } from './DB/db.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { ArtistsModule } from './artists/artists.module';
+import { TracksModule } from './tracks/tracks.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), Database, UsersModule, ArtistsModule],
+  imports: [ConfigModule.forRoot(), Database, UsersModule, ArtistsModule, TracksModule],
   controllers: [],
   providers: [],
 })
